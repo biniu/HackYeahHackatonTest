2184f57ef626105c350e38039afcb26ebc207c00
grapek9 <zerex93@gmail.com>
2020-11-28 23:02:45 +0100

    credits scene, menu scene , scripts to handle stuff

---
+++ Assets/Scripts/CreditsHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsHandler : MonoBehaviour
+{
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Credits");
+        }
+    }
+}
---
+++ Assets/Scripts/GoMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoMenu : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
